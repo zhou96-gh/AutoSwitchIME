94edbbedc68baea7f9965b43de5139ce081a302a
zhou <wy_zhou96@163.com>
2026-05-23 15:58:33 +0800

    feat(intellij): 创建 IntelliJ 插件模块骨架

---
+++ intellij/build.gradle.kts
@@ -0,0 +1,55 @@
+// intellij/build.gradle.kts
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+
+plugins {
+    id("org.jetbrains.kotlin.jvm") version "2.3.0"
+    id("org.jetbrains.intellij.platform") version "2.8.0"
+}
+
+group = "com.rimevim"
+version = rootProject.version
+
+repositories {
+    mavenCentral()
+    intellijPlatform {
+        defaultRepositories()
+    }
+}
+
+dependencies {
+    implementation(project(":core"))
+
+    intellijPlatform {
+        phpstorm("2026.1")
+        plugins("IdeaVIM:2.35.2")
+        pluginVerifier()
+        zipSigner()
+    }
+
+    testImplementation("org.junit.jupiter:junit-jupiter:5.10.2")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+intellijPlatform {
+    pluginConfiguration {
+        version.set("${project.version}")
+        ideaVersion {
+            sinceBuild.set("261")
+            untilBuild.set("261.*")
+        }
+    }
+}
+
+tasks {
+    compileKotlin {
+        compilerOptions {
+            jvmTarget.set(JvmTarget.JVM_17)
+        }
+    }
+    compileJava {
+        options.release.set(17)
+    }
+    test {
+        useJUnitPlatform()
+    }
+}
